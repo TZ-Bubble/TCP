75318f401ff483ace9ec4a0fffc8656e0210515a
tzbubble <1140887835@qq.com>
2022-06-15 01:52:58 +0800

    add_jj

--- JavaServer2/src/Server.java
+++ JavaServer2/src/Server.java
@@ -19,7 +19,7 @@ public class Server {
     public static void main(String[] args) {
         ServerSocket serverSocket= null;
         try {
-            serverSocket = new ServerSocket(8889);
+            serverSocket = new ServerSocket(8889);  //创建绑定到8889端口的服务套接字
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -27,10 +27,10 @@ public class Server {
         while (true){
             try {
                 // 未连通前线程阻塞，连通后开启一个socket通道线程后继续监听8888端口
-                Socket socket = serverSocket.accept();
+                Socket socket = serverSocket.accept();  //accept方法返回一个套接字
                 System.out.println(socket.getInetAddress().getHostAddress()
-                        + "连接进入");
-                new ServerThread(socket).start();
+                        + "连接进入");                   //连接进入时，打印出返回的套接字的IP
+                new ServerThread(socket).start();       //start方法启动线程
 
             } catch (IOException e) {
                 e.printStackTrace();
--- JavaServer2/src/ServerThread.java
+++ JavaServer2/src/ServerThread.java
@@ -7,21 +7,21 @@ import java.util.ArrayList;
  * 服务器线程处理类
  */
 
-public class ServerThread extends Thread {
+public class ServerThread extends Thread {  //ServerThread类，继承Thread，多线程
     //用于标识手机还是单片机,0为手机，1为单片机
-    private static ArrayList<PrintWriter> phoneList = new ArrayList<PrintWriter>();
+    private static ArrayList<PrintWriter> phoneList = new ArrayList<PrintWriter>(); //PrintWriter的动态数组
     private static ArrayList<PrintWriter> MCUList = new ArrayList<PrintWriter>();
 
-    public String what = "";
-    private Socket socket = null;
+    public String what = "";        //标识MCU还是PHONE
+    private Socket socket = null;   //传入的socket
     private BufferedReader bufferedReader;
     private PrintWriter printWriter;
 
-    public ServerThread(Socket socket) throws IOException{
+    public ServerThread(Socket socket) throws IOException{  //构造方法，传入一个socket
         this.bufferedReader = new BufferedReader(new InputStreamReader(socket
-                .getInputStream()));
-        this.printWriter = new PrintWriter(socket.getOutputStream());
-        String info = bufferedReader.readLine();
+                .getInputStream()));    //取入数据(使用指向启动线程的socket的输入流对bufferedReader初始化)
+        this.printWriter = new PrintWriter(socket.getOutputStream());   //传出数据(...)
+        String info = bufferedReader.readLine();    //读取一行
         if (info.contains("MCU")){
             MCUList.add(printWriter);
             this.what = "MCU";
@@ -31,13 +31,21 @@ public class ServerThread extends Thread {
         }
     }
 
-    public void run(){
+    public void run(){      //线程的功能代码
 
         String info = null;
         try {
 
             while((info = bufferedReader.readLine()) != null){
-		System.out.println("收到数据：" + info);
+		System.out.println("收到数据：" + info);     //读入一行非空数据时打印
+
+
+		//http://147.8.133.247/api/icore/gps?hwid={}&lat=22.234&lng=114.123&height=50&numsat=5
+        //request.get
+
+                if()
+
+
                 if(this.what == "MCU"){
                     System.out.println("MCU发来数据：" + info);
                     if(info.contains("__DATA_")){
